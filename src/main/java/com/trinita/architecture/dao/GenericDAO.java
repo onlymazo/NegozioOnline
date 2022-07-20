32bac0fef9f946d99c62eacc4aff4cf0317e08a8
LyssaBS <lucreziacaso.p@gmail.com>
2022-07-20 12:21:04 +0200

    GenericDAO

---
+++ src/main/java/com/trinita/architecture/dao/GenericDAO.java
@@ -0,0 +1,13 @@
+package com.trinita.architecture.dao;
+
+import java.sql.Connection;
+import java.sql.SQLException;
+
+public interface GenericDAO<T> {
+	void create(Connection conn, T entity) throws SQLException;
+	void update(Connection conn, T Entity) throws SQLException;
+	void delete(Connection conn, long id) throws SQLException;
+	T getById(Connection conn, long id) throws SQLException;
+	T [] getAll(Connection conn) throws SQLException;
+
+}
