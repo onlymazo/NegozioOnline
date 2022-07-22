824875d1fa6f4414614ec880fb17b7774e0b42a6
sat00 <sat00@DESKTOP-I6DU1RD>
2022-07-22 19:27:35 +0200

    aggiornato utenteservice per funzionamento angular(si spera)

--- src/main/java/com/trinita/businesscomponent/service/UtenteService.java
+++ src/main/java/com/trinita/businesscomponent/service/UtenteService.java
@@ -1,10 +1,12 @@
 package com.trinita.businesscomponent.service;
 
-import java.sql.Date;
 import java.sql.SQLException;
+import java.text.ParseException;
 
 import javax.naming.NamingException;
 import javax.transaction.Transactional;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.DELETE;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -12,13 +14,8 @@ import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-
 import com.trinita.businesscomponent.UtenteBC;
 import com.trinita.businesscomponent.model.Utente;
-
-import javax.ws.rs.Consumes;
-import javax.ws.rs.DELETE;
-import javax.ws.rs.FormParam;
 
 @Path("/utenteservice")
 public class UtenteService {
@@ -26,53 +23,34 @@ public class UtenteService {
 	@POST
 	@Path("/create")
 	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Transactional
-	public Utente createUtente(
-		@FormParam("nome") String nome,
-		@FormParam("nascita") Date nascita,
-		@FormParam("username") String username,
-		@FormParam("password") String password) throws SQLException, NamingException {
-		Utente u = new Utente();
-		u.setNome(nome);
-		u.setNascita(nascita);
-		u.setUsername(username);
-		u.setPassword(password);
+	public Utente createUtente(Utente utente) 
+			throws NamingException, SQLException, ParseException {
 		UtenteBC uBC = new UtenteBC();
-		uBC.create(u);
-		return u;
+		uBC.create(utente);
+		return utente;
 	}
 	
 	@PUT
 	@Path("/update")
 	@Produces(MediaType.APPLICATION_JSON)
-	@Consumes(MediaType.APPLICATION_FORM_URLENCODED)
+	@Consumes(MediaType.APPLICATION_JSON)
 	@Transactional
-	public Utente updateUtente(
-		@FormParam("id_user") long id_user,
-		@FormParam("nome") String nome,
-		@FormParam("nascita") Date nascita,
-		@FormParam("username") String username,
-		@FormParam("password") String password) throws SQLException, NamingException {
-		Utente u = new Utente();
-		u.setId(id_user);
-		u.setNome(nome);
-		u.setNascita(nascita);
-		u.setUsername(username);
-		u.setPassword(password);
+	public Utente updateUtente(Utente utente) 
+			throws NamingException, SQLException, ParseException {
 		UtenteBC uBC = new UtenteBC();
-		uBC.update(u);
-		return u;
+		uBC.update(utente);
+		return utente;
 	}
 	
 	@DELETE
-	@Path("/delete/{id_utente}")
-	@Produces(MediaType.TEXT_HTML)
+	@Path("/delete/{id_user}")
+	@Produces(MediaType.APPLICATION_JSON)
 	@Transactional
-	public String deleteUtente(@PathParam("id_utente") long id_utente) throws NamingException, SQLException {
+	public void deleteUtente(@PathParam("id_user") long id) 
+			throws NamingException, SQLException {
 		UtenteBC uBC = new UtenteBC();
-		uBC.delete(id_utente);
-		return "<html><head><title>Risposta</title></head><body>Record eliminato correttamente</body></html>";
-	}
-	
+		uBC.delete(id);
+	}	
 }
